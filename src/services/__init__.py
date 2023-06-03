45964a8a28103f186089a5a040bf71e3628cad33
Juan Benavides <jbbenavidesr@gmail.com>
2023-06-03 04:12:40 +0000

    :building_construction: Simplify structure of service

--- src/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .signature_service import SignatureService
-
-__all__ = ["SignatureService"]
--- src/services/signature_service.py
+++ src/services/signature_service.py
@@ -10,56 +10,50 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import (
 )
 
 
-class SignatureService:
-    """Sign and verify methods"""
+def generate_keypair() -> tuple[Ed25519PrivateKey, Ed25519PublicKey]:
+    """Generate a new keypair.
 
-    @staticmethod
-    def generate_keypair() -> tuple[Ed25519PrivateKey, Ed25519PublicKey]:
-        """Generate a new keypair.
+    Returns:
+        The private key.
+        The public key.
+    """
+    private_key = Ed25519PrivateKey.generate()
+    public_key = private_key.public_key()
 
-        Returns:
-            The private key.
-            The public key.
-        """
+    return private_key, public_key
+
+def sign(
+    message: bytes, private_key: Ed25519PrivateKey | None = None
+) -> tuple[bytes, Ed25519PrivateKey]:
+    """Sign a message.
+
+    Args:
+        message: The message to sign.
+        private_key: The private key to use for signing. If not provided, 
+            a new key will be generated.
+
+    Returns:
+        The signature.
+        The private key used for signing.
+    """
+    if private_key is None:
         private_key = Ed25519PrivateKey.generate()
-        public_key = private_key.public_key()
 
-        return private_key, public_key
+    return private_key.sign(message), private_key
 
-    @staticmethod
-    def sign(
-        message: bytes, private_key: Ed25519PrivateKey | None = None
-    ) -> tuple[bytes, Ed25519PrivateKey]:
-        """Sign a message.
+def verify(message: bytes, signature: bytes, public_key: Ed25519PublicKey) -> bool:
+    """Verify a signature.
 
-        Args:
-            message: The message to sign.
-            private_key: The private key to use for signing. If not provided, 
-                a new key will be generated.
+    Args:
+        message: The message to verify.
+        signature: The signature to verify.
+        public_key: The public key to use for verification.
 
-        Returns:
-            The signature.
-            The private key used for signing.
-        """
-        if private_key is None:
-            private_key = Ed25519PrivateKey.generate()
-
-        return private_key.sign(message), private_key
-
-    @staticmethod
-    def verify(message: bytes, signature: bytes, public_key: Ed25519PublicKey) -> bool:
-        """Verify a signature.
-
-        Args:
-            message: The message to verify.
-            signature: The signature to verify.
-            public_key: The public key to use for verification.
-
-        Returns:
-            True if the signature is valid, False otherwise.
-        """
-        try:
-            public_key.verify(signature, message)
-            return True
-        except InvalidSignature:
-            return False
+    Returns:
+        True if the signature is valid, False otherwise.
+    """
+    try:
+        public_key.verify(signature, message)
+        return True
+    except InvalidSignature:
+        return False
