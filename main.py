4aa21310e56f0d4ed36baa91e6e9b95d5355cbe7
Cristian Triana <crisdevelop4@gmail.com>
2023-06-02 23:07:43 -0500

    Add sign page to pdf

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+from src.modules.pdf import add_sign_page_pdf
+
+
+if __name__ == '__main__':
+    
+    pag1_pdf = open('pag1.pdf', 'rb')
+    fileobj = add_sign_page_pdf(pag1_pdf, 'Cristian Triana')
